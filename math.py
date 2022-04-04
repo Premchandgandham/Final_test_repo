d1b5b1785c9931d70d0066d1ad9d84d4bd88901e
leelapremchand_gandham <leela_gandham@persistent.com>
2022-04-04 10:14:35 +0530

    Definition added in each function

--- math.py
+++ math.py
@@ -1,9 +1,13 @@
+#implementation Add Fn
 def add(x,y)
     pass
+#implementation Sub Fn
 def subtract(x,y)  
     pass
+#implementation multiple Fn
 def multiple(x,y)
     pass
+#implementation divison Fn
 def divison(x,y)
     pass
 
